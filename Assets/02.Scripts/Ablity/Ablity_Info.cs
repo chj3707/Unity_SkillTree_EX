57e0b9c44c3e66da2ea858bb9be47f523a90ec29
chj3707 <chohyunjin4280@gmail.com>
2021-04-19 09:09:48 +0900

    Revert "사용 안하는 스크립트 삭제"
    This reverts commit 06930b55b4626dbaa6d434c19619ae2ae9e60c9a.

---
+++ Assets/02.Scripts/Ablity/Ablity_Info.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class MyAblity
+{
+    public E_AblityType ablity;
+
+    // 생성자
+    public MyAblity()
+    {
+        ablity = E_AblityType.None;
+    }
+
+    // 세팅 함수
+    public void SetAblity(E_AblityType p_ablity)
+    {
+        ablity = p_ablity;
+    }
+}
+
+// 능력창 별로 능력 타입 설정
+public class Ablity_Info : MonoBehaviour
+{
+    public MyAblity m_Ablity;
+
+
+    void Awake()
+    {
+        m_Ablity = new MyAblity(); // 초기화
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+}
